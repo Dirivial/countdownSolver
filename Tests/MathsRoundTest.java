7681c61fa8dd998c276ffd5686cfcb7960a27e6f
Alkade <alexander.yay.wow@gmail.com>
2021-02-21 02:42:28 +0100

    Created a test.

--- Tests/MathsRoundTest.java
+++ Tests/MathsRoundTest.java
@@ -1,8 +1,17 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MathsRoundTest {
+
+    private MathsRound mr;
+
+    @BeforeEach
+    void beforeEach() {
+        mr = new MathsRound();
+    }
 
     @Test
     void calc() {
@@ -14,5 +23,9 @@ class MathsRoundTest {
 
     @Test
     void calculateOperation() {
+        Assertions.assertEquals(6, mr.calculateOperation("+", 4, 2));
+        Assertions.assertEquals(2, mr.calculateOperation("-", 4, 2));
+        Assertions.assertEquals(8, mr.calculateOperation("*", 4, 2));
+        Assertions.assertEquals(2, mr.calculateOperation("/", 4, 2));
     }
 }
